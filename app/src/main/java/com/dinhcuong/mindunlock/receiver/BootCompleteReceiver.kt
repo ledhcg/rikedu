0c43abcf5f801c139a3de0d8474861087c4ece4e
ledhcg <dinhcuong.firewin99@gmail.com>
2022-07-09 01:53:57 +0300

    Fixed: Autostart

--- app/src/main/java/com/dinhcuong/mindunlock/receiver/BootCompleteReceiver.kt
+++ app/src/main/java/com/dinhcuong/mindunlock/receiver/BootCompleteReceiver.kt
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
+import com.dinhcuong.mindunlock.activity.LockScreenActivity
 import com.dinhcuong.mindunlock.service.LockScreenService
 
 class BootCompleteReceiver: BroadcastReceiver() {
@@ -11,6 +13,16 @@ class BootCompleteReceiver: BroadcastReceiver() {
         when (intent?.action) {
             Intent.ACTION_BOOT_COMPLETED -> {
                 Log.d("[BootCompleteReceiver]","Boot complete")
+                Toast.makeText(
+                    context,
+                    "Startup",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                val intentLockScreen = Intent(context, LockScreenActivity::class.java)
+                intentLockScreen.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intentLockScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                context?.startActivity(intentLockScreen)
                 val intentLS = Intent(context, LockScreenService::class.java)
                 context?.let {
                     val pref = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context)
