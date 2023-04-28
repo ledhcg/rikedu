0754ff180fa41997f4b6003dd01a293e63483b5e
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-04-28 21:35:06 +0300

    Initial commit

--- rike/android/app/src/main/kotlin/com/example/rike/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rike
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- rike/lib/src/constants/file_strings.dart
+++
@@ -1,6 +0,0 @@
-const String logo = "assets/images/logo/logo.png";
-const String avatarDefault = "assets/images/avatar/6.png";
-const String iconMarker = "assets/images/icons/map-marker.png";
-
-const String mapLight = "assets/json/map-light.json";
-const String mapDark = "assets/json/map-dark.json";
