ea301553ab0e059e3dc5c1a37575c24bfc65ec96
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-01 13:54:36 +0300

    Added system colors

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-const rikePrimaryColor = Color(0xFFFF5733);
-const rikeSecondaryColor = Color(0xFF593E67);
+const rikePrimaryColor = Color(0xFF509bf8);
+const rikeSecondaryColor = Color(0xFFFF5733);
 const rikeAccentColor = Color(0xFF84495F);
+const rikeDisabledColor = Color(0xFFcccccc);
+const rikeOutsideColor = Color(0xFFbfbfbf);
+
+const rikeTextColorLight = Color(0xFF080808);
 
 const rikeLightColor = Color(0xFFF4F4F4);
 const rikeDarkColor = Color(0xFF272727);
@@ -11,3 +15,31 @@ const rikeIndicatorBubble = Color(0xFF39B54A);
 const rikeGreenBattery = Color(0xFF39B54A);
 const rikeYellowBattery = Color(0xFFFFC300);
 const rikeRedBattery = Color(0xFFFF0000);
+
+// SYSTEM COLORS
+
+const rikePrimaryColorLight = Color(0xFF509bf8);
+const rikeOnPrimaryColorLight = Color(0xFFffffff);
+const rikePrimaryContainerColorLight = Color(0xFFeeeded);
+const rikeSecondaryColorLight = Color(0xFFFF5733);
+const rikeOnSecondaryColorLight = Color(0xFFfcfff7);
+const rikeScrimColoLight = Color(0xFFdbdada);
+const rikeSurfaceColoLight = Color(0xFFbdbcbc);
+const rikeOnSurfaceColoLight = Color(0xFF1d2125);
+const rikeBackgroundColorLight = Color(0xFFffffff);
+const rikeOnBackgroundColorLight = Color(0xFF080808);
+const rikeErrorColorLight = Color(0xFFea3321);
+const rikeOnErrorColorLight = Color(0xFFffffff);
+
+const rikePrimaryColorDark = Color(0xFF97d1fc);
+const rikeOnPrimaryColorDark = Color(0xFF1d2125);
+const rikePrimaryContainerColorDark = Color(0xFF252526);
+const rikeSecondaryColorDark = Color(0xFFfc876d);
+const rikeOnSecondaryColorDark = Color(0xFF1d2125);
+const rikeScrimColoDark = Color(0xFF3d3d3d);
+const rikeSurfaceColoDark = Color(0xFF4e4e4e);
+const rikeOnSurfaceColoDark = Color(0xFFfcfff7);
+const rikeBackgroundColorDark = Color(0xFF1d2125);
+const rikeOnBackgroundColorDark = Color(0xFFfcfff7);
+const rikeErrorColorDark = Color(0xFFCF6679);
+const rikeOnErrorColorDark = Color(0xFF1d2125);
