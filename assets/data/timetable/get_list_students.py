6196052b3cdaecf701950ea66936c654d8fbd460
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-06 14:53:30 +0300

    Update genarate data users

---
+++ assets/data/timetable/get_list_students.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Mở file text
+with open('constants/students.txt', 'r', encoding="utf-8") as file:
+    # Đọc các dòng của file và lưu vào một list
+    lines = file.readlines()
+
+# Tạo một array từ list các dòng
+array = [line.rstrip() for line in lines]
+
+# Xuất ra array
+print(array)
