cb0b37d70b190b08fa605a15a2f06d8ddb0b6aaa
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-20 01:26:38 +0300

    Update login_page.dart

--- lib/src/features/authentication/views/login_page.dart
+++ lib/src/features/authentication/views/login_page.dart
@@ -1,6 +1,8 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rikedu/src/features/authentication/controllers/auth_controller.dart';
+import 'package:rikedu/src/utils/constants/files_constants.dart';
 import 'package:rikedu/src/utils/constants/sizes_constants.dart';
 import 'package:rikedu/src/utils/widgets/loading_widget.dart';
 
@@ -13,50 +15,65 @@ class LoginPage extends GetView<AuthController> {
       body: Obx(
         () => controller.isLoading
             ? const LoadingWidget()
-            : SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(SizesConst.P0),
-                  child: Form(
-                    key: controller.formKey,
+            : Center(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(SizesConst.P3),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        TextFormField(
-                          onChanged: (value) => controller.setEmail(value),
-                          controller: controller.emailController,
-                          decoration: InputDecoration(
-                            labelText: 'Email'.tr,
-                          ),
-                          validator: controller.emailValidator,
+                        Container(
+                          height: 200,
+                          width: 200,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: SizesConst.P3),
+                          child: Image.asset(FilesConst.LOGO_ROUNDED),
                         ),
-                        const SizedBox(height: 16.0),
-                        TextFormField(
-                          onChanged: (value) => controller.setPassword(value),
-                          controller: controller.passwordController,
-                          decoration: InputDecoration(
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                controller.isObscure
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
+                        Form(
+                          key: controller.formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              TextFormField(
+                                onChanged: (value) =>
+                                    controller.setEmail(value),
+                                controller: controller.emailController,
+                                decoration: InputDecoration(
+                                  labelText: 'Email'.tr,
+                                ),
+                                validator: controller.emailValidator,
                               ),
-                              onPressed: controller.toggleObscure,
-                            ),
-                            labelText: 'Password'.tr,
+                              const SizedBox(height: 16.0),
+                              TextFormField(
+                                onChanged: (value) =>
+                                    controller.setPassword(value),
+                                controller: controller.passwordController,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      controller.isObscure
+                                          ? FluentIcons.eye_off_24_filled
+                                          : FluentIcons.eye_24_filled,
+                                    ),
+                                    onPressed: controller.toggleObscure,
+                                  ),
+                                  labelText: 'Password'.tr,
+                                ),
+                                obscureText: controller.isObscure,
+                                validator: controller.passwordValidator,
+                              ),
+                              const SizedBox(height: 16.0),
+                              FilledButton(
+                                onPressed: controller.login,
+                                child: Text('Login'.tr),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text('Forgot password?'.tr),
+                              ),
+                            ],
                           ),
-                          obscureText: controller.isObscure,
-                          validator: controller.passwordValidator,
-                        ),
-                        const SizedBox(height: 16.0),
-                        ElevatedButton(
-                          onPressed: controller.login,
-                          child: Text('Login'.tr),
-                        ),
-                        const SizedBox(height: SizesConst.P0),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text('forgotPassword'.tr),
                         ),
                       ],
                     ),
