40ec069e8444e2ce5cd21719ab177b45da1179ed
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-19 17:15:22 +0300

    Update Bindings

--- lib/src/features/settings/bindings/settings_binding.dart
+++ lib/src/features/settings/bindings/settings_binding.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
+import 'package:rikedu/src/features/settings/controllers/language_controller.dart';
 import 'package:rikedu/src/features/settings/controllers/setting_controller.dart';
+import 'package:rikedu/src/features/settings/controllers/theme_controller.dart';
 
 class SettingsBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<SettingsController>(() => SettingsController());
+    Get.lazyPut<ThemeController>(() => ThemeController());
+    Get.lazyPut<LanguageController>(() => LanguageController());
   }
 }
