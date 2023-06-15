84067eb701f9e81f1954bc8928be172965f09c3d
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-06-15 21:21:04 +0300

    Update files_constants.dart

--- lib/src/utils/constants/files_constants.dart
+++ lib/src/utils/constants/files_constants.dart
@@ -12,6 +12,9 @@ class FilesConst {
   static const String MAP_LIGHT = "assets/json/map-light.json";
   static const String MAP_DARK = "assets/json/map-dark.json";
 
+  static const String LOTTIE_LOADING = "assets/json/lottie-loading.json";
+  static const String LOTTIE_SUCCESS = "assets/json/lottie-success.json";
+
   static const String ON_BOARDING_1 =
       "assets/images/on_boarding/on-boarding-1.png";
   static const String ON_BOARDING_2 =
